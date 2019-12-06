5a9c85d64ff681d2cb0ce98b0314c211a484c033
jmtoniolo <55120550+jmtoniolo@users.noreply.github.com>
2019-12-05 20:41:29 -0500

    xml validation support

--- init.el
+++ init.el
@@ -79,6 +79,19 @@
     (make-directory "~/.emacs.d/plugins/"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General Tweaks
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; nXML mode customization
+(add-to-list 'auto-mode-alist '("\\.xsd\\'" . xml-mode))
+(add-to-list 'auto-mode-alist '("\\.xslt\\'" . xml-mode))
+(add-hook 'nxml-mode-hook
+	  '(lambda ()
+	     (make-local-variable 'indent-tabs-mode)
+	     (setq indent-tabs-mode nil)
+	     (add-to-list 'rng-schema-locating-files
+			  "~/.emacs.d/nxml-schemas/schemas.xml")))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; General Tweaks
